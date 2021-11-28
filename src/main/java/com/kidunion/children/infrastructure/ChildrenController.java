36102e90bef6875c6af2da1a28f615abaee7423f
Frederick Perez <frederickperezdelacruz@outlook.com>
2021-11-28 09:29:39 -0400

    add Upgrades

--- src/main/java/com/kidunion/children/infrastructure/ChildrenController.java
+++ src/main/java/com/kidunion/children/infrastructure/ChildrenController.java
@@ -1,8 +1,8 @@
 package com.kidunion.children.infrastructure;
 
 import com.kidunion.children.domain.Children;
-import com.kidunion.utilities.CrudGeneric;
-import com.kidunion.utilities.FindByValue;
+import com.kidunion.members.domain.Members;
+import com.kidunion.utilities.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,14 +14,20 @@ import java.util.List;
 @RestController
 @RequestMapping("kidunion")
 public class ChildrenController {
-
-    private final CrudGeneric<Children> crudGeneric;
     private final FindByValue<Children> findByValue;
+    private final SaveEntity<Children> saveEntity;
+    private final DeleteEntity<Children> deleteMember;
+    private final UpdateEntity<Children> updateEntity;
+    private final FindAllEntity<Children> findAllEntity;
 
-    @Autowired
-    public ChildrenController(CrudGeneric<Children> crudGeneric , FindByValue<Children> findByValue ) {
-        this.crudGeneric = crudGeneric;
+    public ChildrenController(FindByValue<Children> findByValue, SaveEntity<Children> saveEntity,
+                              DeleteEntity<Children> deleteMember, UpdateEntity<Children> updateEntity,
+                              FindAllEntity<Children> findAllEntity) {
         this.findByValue = findByValue;
+        this.saveEntity = saveEntity;
+        this.deleteMember = deleteMember;
+        this.updateEntity = updateEntity;
+        this.findAllEntity = findAllEntity;
     }
 
     /**
@@ -29,27 +35,29 @@ public class ChildrenController {
      */
     @GetMapping("/Children")
     public List<Children> findAll() {
-        return crudGeneric.findAll();
+        return findAllEntity.findAll();
     }
 
     /**
      * This method saves the children
+     *
      * @param children
      */
     @PostMapping("/save")
     public ResponseEntity<String> save(@RequestBody Children children) {
-        this.crudGeneric.save(children);
+        this.saveEntity.save(children);
         return ResponseEntity.status(HttpStatus.CREATED).body("Successfully created");
     }
 
     /**
      * This method filters a list of members according to his firstName
+     *
      * @param firstName
      * @return list of Children
      */
     @GetMapping("/findByFirstName/{firstName}")
-    public List<Children> findByFirstName(@PathVariable("firstName") String firstName){
-        if (firstName.isEmpty()){
+    public List<Children> findByFirstName(@PathVariable("firstName") String firstName) {
+        if (firstName.isEmpty()) {
             return new ArrayList<>();
         }
         return findByValue.findByValue(firstName);
